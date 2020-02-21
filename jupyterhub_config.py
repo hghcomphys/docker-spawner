b9a7031710b2d23c28b58144917b5a6df6f12fb2
Hossein <hgh.comphys@gmail.com>
2020-02-21 09:17:47 +0100

    set docker notebook directory and add volume for user data

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -21,3 +21,9 @@ c.DockerSpawner.network_name = 'jupyterhub'
 
 # delete containers when the stop
 c.DockerSpawner.remove = True
+
+import os
+# user data persistence
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR')
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
