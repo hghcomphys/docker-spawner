608cbdf6ad0901db63f55bc193cb5fb02788da3d
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2020-02-27 10:11:21 +0100

    workable version on MONA cluster (only CPU)

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -2,29 +2,42 @@ import os
 
 # dummy for testing. Don't use this in production!
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.DummyAuthenticator.password = "testpass"
+c.Authenticator.admin_users = {'hossein'}
+c.JupyterHub.admin_access = True
+#c.Spawner.args = ['--allow-root']
+
 
 # launch with docker
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DummyAuthenticator.password = "testpass"
 
 # we need the hub to listen on all ips when it is in a container
-#c.JupyterHub.hub_ip = '0.0.0.0'
-#c.JupyterHub.port = 8080
+c.JupyterHub.hub_ip = '0.0.0.0'
+#c.JupyterHub.hub_port = 8080
+#c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.port = 8080
+
 
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
 c.JupyterHub.hub_connect_ip = 'jupyterhub'
 # tell the user containers to connect to our docker network
-c.DockerSpawner.network_name = 'jupyterhub'
+c.DockerSpawner.network_name = 'jupyterhub-network'
+c.DockerSpawner.debug = True
+c.DockerSpawner.use_internal_ip = True
+#c.Spawner.ip = '127.0.0.1'
+c.DockerSpawner.extra_host_config = { 'network_mode': 'jupyterhub-network' }
+c.DockerSpawner.extra_start_kwargs = { 'network_mode': 'jupyterhub-network' }
+
 
 ## Path to SSL certificate file for the public facing interface of the proxy
 #
 #  When setting this, you should also set ssl_key
-#c.JupyterHub.ssl_cert = '/srv/jupyterhub/mycert.pem'
+c.JupyterHub.ssl_cert = '/srv/jupyterhub/mycert.pem'
 ## Path to SSL key file for the public facing interface of the proxy
 #
 #  When setting this, you should also set ssl_cert
-#c.JupyterHub.ssl_key = '/srv/jupyterhub/mykey.key'
+c.JupyterHub.ssl_key = '/srv/jupyterhub/mykey.key'
 
 
 # pick a docker image. This should have the same version of jupyterhub
@@ -35,7 +48,7 @@ c.DockerSpawner.image_whitelist = {
     "tensorflow-v2-cpu": "notebook:tensorflow-v2-cpu",
 }
 # user account inside the conainer
-#c.DockerSpawner.extra_create_kwargs = {'user': 'root'}
+#c.DockerSpawner.extra_create_kwargs = {'user': 'user'}
 #c.Spawner.environment = {'GRANT_SUDO': 'yes'}
 
 # CPU
@@ -43,7 +56,8 @@ c.Spawner.mem_limit = '2G'
 c.Spawner.cpu_limit = 1
 
 # GPU 
-c.DockerSpawner.extra_host_config = {'runtime': 'nvidia'}
+#c.DockerSpawner.extra_host_config = {'runtime': 'nvidia'}
+
 
 # delete containers when the stop
 c.DockerSpawner.remove = True
