c97a4112a719cb59c921457a087975174c0f01d1
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2020-02-27 10:04:18 +0100

    print out docker outputs

--- images/make-all-images.sh
+++ images/make-all-images.sh
@@ -3,6 +3,5 @@ for dir in */
 do
     img=${dir%*/}   
     echo Build ${img} image as notebook:${img}
-    docker image build -f ${img}/Dockerfile . -t notebook:${img} >> /dev/null 
-
+    docker image build -f ${img}/Dockerfile . -t notebook:${img} #>> /dev/null 
 done
